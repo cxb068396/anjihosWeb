14f5aa56c64fe2227189a88a330dc7cd2f575863
定宏施 <377089304@qq.com>
2019-06-21 16:02:41 +0800

    get all 4 and 5 roles function

--- src/views/Personnel/DoctorteamAddPage.vue
+++ src/views/Personnel/DoctorteamAddPage.vue
@@ -72,7 +72,7 @@
       <el-form-item
         label="选择健康管家"
       >
-        <template>
+        <!-- <template>
           <el-select v-model="choosed4" placeholder="请选择健康管家">
             <el-option
               v-for="item in role4"
@@ -81,13 +81,19 @@
               :value="item.id">
             </el-option>
           </el-select>
-        </template>
+        </template> -->
+         <el-cascader
+          :options="role4"
+          v-model="choosed4"
+          @change="handleChange4"
+          >
+        </el-cascader>
       </el-form-item>
 
       <el-form-item
         label="选择上门服务人员"
       >
-        <template>
+        <!-- <template>
           <el-select v-model="choosed5" placeholder="请选择上门服务人员">
             <el-option
               v-for="item in role5"
@@ -96,7 +102,13 @@
               :value="item.id">
             </el-option>
           </el-select>
-        </template>
+        </template> -->
+        <el-cascader
+          :options="role5"
+          v-model="choosed5"
+          @change="handleChange5"
+          >
+        </el-cascader>
       </el-form-item>
       
 
@@ -123,8 +135,8 @@ export default {
       choosed1: '',
       choosed2: '',
       choosed3: '',
-      choosed4: '',
-      choosed5: '',
+      choosed4: [],
+      choosed5: [],
       
 
    
@@ -132,6 +144,65 @@ export default {
   },
 
   methods: {
+    //获取可重复的人员列表
+    getRepeatableList(){
+      let that = this
+      this.axios
+        .get(
+          "/company",
+          {
+            params: {
+              size:1000,
+              page: this.currentPage
+            }
+          }
+        )
+        .then(response => {
+          that.role4 = JSON.parse(JSON.stringify(response.data.data.data))
+          that.role5 = JSON.parse(JSON.stringify(response.data.data.data))
+          console.log(response.data.data.data)
+          console.log(this.role5)
+          that.role4.map(item => item.label = item.name)
+          that.role4.map(item => item.value = item.name)
+          that.role4.map(
+            function(item){
+              that.axios.get('worker?worker_role_id=4&company_id='+item.id)
+              .then(res => {
+                item.children = res.data.data.data
+                // console.log(item.children)
+                // item.children = item.children.filter(item4 => item4.worker_role_id == 4)
+                // console.log(item.children)
+                item.children.map(item => item.label = item.name)
+                item.children.map(item => item.value = item.id)
+              })
+            }
+          )
+          console.log(this.role4)
+          console.log(this.role5)
+          that.role5.map(item => item.label = item.name)
+          that.role5.map(item => item.value = item.name)
+          that.role5.map(
+            function(item){
+              that.axios.get('worker?worker_role_id=5&company_id='+item.id)
+              .then(res => {
+                item.children = res.data.data.data
+                // console.log(item.children)
+                // item.children = item.children.filter(item5 => item5.worker_role_id == 5)
+                // console.log(item.children)
+                item.children.map(item => item.label = item.name)
+                item.children.map(item => item.value = item.id)
+              })
+            }
+          )
+          
+          // console.log(this.role5)
+          
+          
+        });
+    },
+    getWorkerList(){
+
+    },
     getList() {
       let userInfo = JSON.parse(localStorage.getItem("userInfo"));
       let company_id = userInfo.company_id;
@@ -147,7 +218,7 @@ export default {
         )
         .then(response => {
           // this.tableData = response.data.data.data;
-          console.log(response.data.data.data)
+          // console.log(response.data.data.data)
           this.workers = response.data.data.data
           this.getRoles()
           // this.total = response.data.data.count;
@@ -159,25 +230,32 @@ export default {
       this.role1 = this.workers.filter(item=>item.worker_role_id == 1)
       this.role2 = this.workers.filter(item=>item.worker_role_id == 2)
       this.role3 = this.workers.filter(item=>item.worker_role_id == 3)
-      this.role4 = this.workers.filter(item=>item.worker_role_id == 4)
-      this.role5 = this.workers.filter(item=>item.worker_role_id == 5)
+      // this.role4 = this.workers.filter(item=>item.worker_role_id == 4)
+      // this.role5 = this.workers.filter(item=>item.worker_role_id == 5)
     },
-  
+    handleChange4(value){
+      // console.log(value)
+    },
+    handleChange5(value){
+      // console.log(value)
+    },
     goBackPage() {
       this.$router.go(-1);
     },
   
     submitForm() {
-      console.log(this.choosed1)
-      console.log(this.choosed2)
+      
+      // this.choosed4 = this.choosed4[1]
+      // this.choosed5 = this.choosed5[1]
+      
       let userInfo = JSON.parse(localStorage.getItem("userInfo"));
       
       const params = {
         doctor_id:this.choosed1,
         nurse_id:this.choosed2,
         pharmacist_id:this.choosed3,
-        health_manager_id:this.choosed4,
-        service_staff_id:this.choosed5,
+        health_manager_id:this.choosed4[1],
+        service_staff_id:this.choosed5[1],
         company_id :userInfo.company_id,
       };
 
@@ -208,6 +286,7 @@ export default {
   },
   created(){
     this.getList()
+    this.getRepeatableList()
   }
 };
 </script>
