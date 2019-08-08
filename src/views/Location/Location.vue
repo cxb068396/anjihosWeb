9c199e0dc90cffc72141b21ba43629f06e5c6675
定宏施 <377089304@qq.com>
2019-08-08 11:23:22 +0800

    change timer to 10 seconds

--- src/views/Location/Location.vue
+++ src/views/Location/Location.vue
@@ -136,8 +136,8 @@ export default {
     }
     that.getStreet();
     this.timer = setInterval(function () {
-     // that.people();
-    },60000)
+     that.people();
+    },10000)
   },
   beforeDestroy(){
     clearInterval(this.timer)
@@ -269,7 +269,9 @@ export default {
           that.infoWindow.open(map);
          var btn3 = document.getElementById('btn3');
          let openclick=function(){
+           
            that.showHealth=true;
+           that.infoWindow.clear()
            that.getHealth(marker)
          }
          btn3.onclick=openclick
@@ -512,6 +514,7 @@ export default {
           //onclick事件
           let btnclick = function(){
             that.showHealth=true;
+            that.infoWindow.clear()
            that.getallHealth(marker)
           }
           btn.onclick = btnclick
@@ -519,6 +522,7 @@ export default {
     
           var btn2 = document.getElementById('btn2');
           let showMoreMessage = function(){
+            that.infoWindow.clear()
             that.showHealth=true;
            that.getallHealth(marker)
           }
